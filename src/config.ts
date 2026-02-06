a2fa76491627277e7ad3115b543003a28dc26ae2
aumZax <oodgame.1h@gmail.com>
2026-02-06 11:47:36 +0700

    link

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // endpoints.ts
-const BASE_URL = "https://finances-epa-happiness-mandate.trycloudflare.com"; 
+const BASE_URL = "https://ceremony-bean-championships-features.trycloudflare.com"; 
 // const BASE_URL = "http://localhost:3000";
 
 // const BASE_URL = "https://api-test-pi-rosy.vercel.app";
@@ -22,7 +22,7 @@ const ENDPOINTS = {
   GETPROJECTIMAGES: `${BASE_URL}/project/images`,
   DELETEPROJECT: `${BASE_URL}/deleteProject`,
   UPLOAD_AVATAR: `${BASE_URL}/upload/avatar`,
-  image_url: "https://finances-epa-happiness-mandate.trycloudflare.com/",
+  image_url: "https://ceremony-bean-championships-features.trycloudflare.com/",
   videos: `${BASE_URL}/videos`,
   IMAGE_URL: `${BASE_URL}/`,
   UPLOAD_VIDEO: `${BASE_URL}/upload/video`,
