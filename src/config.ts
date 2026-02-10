06f8827e4b33b3d4d6c695c816471963b4ec4432
aumZax <oodgame.1h@gmail.com>
2026-02-10 16:17:28 +0700

    link

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // endpoints.ts
-// const BASE_URL = "https://wagner-healthcare-east-belkin.trycloudflare.com";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://wagner-healthcare-east-belkin.trycloudflare.com";
+// const BASE_URL = "http://localhost:3000";
 
 // const BASE_URL = "https://api-test-pi-rosy.vercel.app";
 
