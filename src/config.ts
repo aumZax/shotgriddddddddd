040384cd974a58c6f61fbbcf64dbcff2323b8ed8
aumZax <oodgame.1h@gmail.com>
2026-02-12 14:15:46 +0700

    8888

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // endpoints.ts
-// const BASE_URL = "https://brother-casinos-papua-wit.trycloudflare.com";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://brother-casinos-papua-wit.trycloudflare.com";
+// const BASE_URL = "http://localhost:3000";
 
 // const BASE_URL = "https://api-test-pi-rosy.vercel.app";
 
