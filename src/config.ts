a51052c593a4518d1331b269ee64c4944e948338
aumZax <oodgame.1h@gmail.com>
2026-02-13 19:14:47 +0700

    link

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // endpoints.ts
-// const BASE_URL = "https://diana-prisoners-negotiation-levy.trycloudflare.com";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://diana-prisoners-negotiation-levy.trycloudflare.com";
+// const BASE_URL = "http://localhost:3000";
 
 // const BASE_URL = "https://api-test-pi-rosy.vercel.app";
 
