d1f65302616acf273785f697ffda490a4b44039b
aumZax <oodgame.1h@gmail.com>
2026-02-23 11:49:47 +0700

    link

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // endpoints.ts
-const BASE_URL = "https://pick-handbags-waiver-lodge.trycloudflare.com";
+const BASE_URL = "https://spoke-creates-vegetable-just.trycloudflare.com";
 // const BASE_URL = "http://localhost:3000";
 
 // const BASE_URL = "https://api-test-pi-rosy.vercel.app";
@@ -22,7 +22,7 @@ const ENDPOINTS = {
   GETPROJECTIMAGES: `${BASE_URL}/project/images`,
   DELETEPROJECT: `${BASE_URL}/deleteProject`,
   UPLOAD_AVATAR: `${BASE_URL}/upload/avatar`,
-  image_url: "https://pick-handbags-waiver-lodge.trycloudflare.com/",
+  image_url: "https://spoke-creates-vegetable-just.trycloudflare.com/",
   videos: `${BASE_URL}/videos`,
   IMAGE_URL: `${BASE_URL}/`,
   UPLOAD_VIDEO: `${BASE_URL}/upload/video`,
