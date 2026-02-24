2a1d4c105a344dd581a1ed6744c4d34bdf898a38
aumZax <oodgame.1h@gmail.com>
2026-02-24 11:33:20 +0700

    AAA

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // endpoints.ts
-const BASE_URL = "https://spoke-creates-vegetable-just.trycloudflare.com";
+const BASE_URL = "https://acting-consolidated-yamaha-fair.trycloudflare.com";
 // const BASE_URL = "http://localhost:3000";
 
 
@@ -23,7 +23,7 @@ const ENDPOINTS = {
   GETPROJECTIMAGES: `${BASE_URL}/project/images`,
   DELETEPROJECT: `${BASE_URL}/deleteProject`,
   UPLOAD_AVATAR: `${BASE_URL}/upload/avatar`,
-  image_url: "https://spoke-creates-vegetable-just.trycloudflare.com/",
+  image_url: "https://acting-consolidated-yamaha-fair.trycloudflare.com/",
   videos: `${BASE_URL}/videos`,
   IMAGE_URL: `${BASE_URL}/`,
   UPLOAD_VIDEO: `${BASE_URL}/upload/video`,
