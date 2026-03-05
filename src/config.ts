01ecde09e08be1f79dbbd28fffba76c7a452f9db
aumZax <oodgame.1h@gmail.com>
2026-03-05 12:14:31 +0700

    +

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // endpoints.ts
-const BASE_URL = "https://eds-scientist-clarke-acids.trycloudflare.com";
+const BASE_URL = "https://demo-newest-spyware-styles.trycloudflare.com";
 // const BASE_URL = "http://localhost:3000";
 
 // maxaaaaa
@@ -22,7 +22,7 @@ const ENDPOINTS = {
   UPLOAD: `${BASE_URL}/upload`,
   GETPROJECTIMAGES: `${BASE_URL}/project/images`,
   DELETEPROJECT: `${BASE_URL}/deleteProject`,
-  image_url: "https://eds-scientist-clarke-acids.trycloudflare.com/",
+  image_url: "https://demo-newest-spyware-styles.trycloudflare.com/",
   videos: `${BASE_URL}/videos`,
   IMAGE_URL: `${BASE_URL}/`,
   UPLOAD_VIDEO: `${BASE_URL}/upload/video`,
